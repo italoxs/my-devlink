3c3ec17b45259874d1a4822fd9301bfdc85278fa
Italo Sales <italo.x.sales@hotmail.com>
2023-01-21 03:25:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light')
+
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+
+  // mudar a imagem
+  // const img = document.querySelector("profile img")
+
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "./assests/avatar-light.png")
+  // } else {
+  //   img.setAttribute("src", "./assests/avatar.png")
+  // }
+  
+}
